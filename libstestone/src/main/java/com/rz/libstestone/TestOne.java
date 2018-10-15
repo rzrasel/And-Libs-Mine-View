7b521bbe98db07c0573d7e51d8c1b51dcb100362
Rz Rasel <developer@Shurjo.local>
2018-10-15 16:09:54 +0600

    And Libs Mine View V-100.00.01 - 2018-10-15 16:09:54

--- libstestone/src/main/java/com/rz/libstestone/TestOne.java
+++ libstestone/src/main/java/com/rz/libstestone/TestOne.java
@@ -5,3 +5,8 @@ public class TestOne {
         System.out.println("Hi I am from MashUp");
     }
 }
+//https://www.raywenderlich.com/52-building-an-android-library-tutorial
+//https://medium.freecodecamp.org/how-modularisation-affects-build-time-of-an-android-application-43a984ce9968
+//https://medium.com/mindorks/distribute-multiple-module-library-on-bintray-for-java-and-android-developers-212216bc1aa7
+//https://github.com/quangctkm9207/multi-library-bintray
+//https://rominirani.com/gradle-tutorial-part-3-multiple-java-projects-5b1c4d1fbd8d
